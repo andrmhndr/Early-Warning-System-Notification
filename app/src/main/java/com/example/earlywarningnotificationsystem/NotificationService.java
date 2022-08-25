5ff5a1277d880064866532afc8bd72cfc9f63bb2
andrmhndr <66835454+andrmhndr@users.noreply.github.com>
2022-08-25 15:46:22 +0700

    final

--- app/src/main/java/com/example/earlywarningnotificationsystem/NotificationService.java
+++ app/src/main/java/com/example/earlywarningnotificationsystem/NotificationService.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
